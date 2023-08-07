5164b3f93422b82329755e53744e2c45a35b5fe8
Altana <altanagrishkina@gmail.com>
2023-08-07 12:36:33 +0200

    Compito 1: Pages & Routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
     <nuxt-link to="/users">Users</nuxt-link>
+    <nuxt-link to="/products">Products</nuxt-link>
   </section>
 
 </template>
---
+++ pages/products/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>A single product, with ID: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+  validate(data){
+    // console.log(data)
+    // return true
+
+    // return data.params.id == 1
+
+    return /^\d+$/.test(data.params.id);
+  }
+}
+</script>
