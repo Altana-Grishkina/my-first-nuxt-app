c17355fd08b7ecbb66d048d89ffe983c14fb8312
Altana <altanagrishkina@gmail.com>
2023-09-06 15:23:29 +0200

    Creating the Main Sctions(Pages)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <Header />
     <nuxt/>
   </div>
 </template>
 
 <script>
-import Header  from '@/components/Header'
-export default {
-  components: {
-    Header
-  }
-}
+
 </script>
 
 <style>
